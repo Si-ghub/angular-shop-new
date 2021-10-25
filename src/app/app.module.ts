b49a6a54b9c124c0faea66c46efd1f7f2565a380
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-10-25 21:06:21 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { ItemComponent } from './components/item/item.component';
+import { CartComponent } from './components/cart/cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemListComponent,
+    ItemComponent,
+    CartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
